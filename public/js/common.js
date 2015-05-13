e1fd031746f11e915c4c20b87357bd6ce76d2d18
Sergey <rudserg@gmail.com>
2015-05-13 13:47:19 +0300

    view curr projects

--- public/js/common.js
+++ public/js/common.js
@@ -1,9 +1,10 @@
 $(function() {
     $( "#tabs" ).tabs();
+    doAction(null, 'projects');
 });
 
 function doAction(btn, action) {
-    $(btn).attr('disabled', true);
+    if (btn) $(btn).attr('disabled', true);
     $.ajax({
         method: "POST",
         url: "/admin/"+action,
@@ -20,9 +21,23 @@ function doAction(btn, action) {
     })
         .done(function( msg ) {
             //console.log(arguments);
-            if (!msg && msg == '') msg = 'Ok';
-            $('#branchResponse').html('<p>'+msg+'</p>'+$('#branchResponse').html());
-            //$('#branchResponse').html('<p>'+msg+'</p>');
-            $(btn).attr('disabled', false);
+            if (action == 'projects') {
+                printProjects(msg);
+            } else {
+                if (!msg && msg == '') msg = 'Ok';
+                $('#branchResponse').html('<p>'+msg+'</p>'+$('#branchResponse').html());
+                //$('#branchResponse').html('<p>'+msg+'</p>');
+                if (action == 'add')
+                    doAction(null, 'projects');
+            }
+            if (btn) $(btn).attr('disabled', false);
         });
+}
+
+function printProjects(msg) {
+    var projects = JSON.parse(msg);
+    $('#currProjects').htm('');
+    for(var i=0; i<projects.length; i++) {
+        $('#currProjects').append('<p>'+projects[i].project+':'+projects[i].projectBranch+'/Uccello:'+projects[i].uccelloBranch+' <a href="http://projects.calypsoid.com:'+projects[i].portWeb+'" target="_blank">открыть</a>'+' <a href="#" onclick="if (confirm(\'Вы уверены?\')) doAction(null, \'delete\', {path:\''+projects[i].path+'\'}); return false;">удалить</a></p>');
+    }
 }
